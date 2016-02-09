9d4dc95ab320c72781bf639fb851755b1fed5644
Rodolphe <rodolphe@alsacreations.fr>
2016-02-09 12:53:28 +0100

    Code source initial et readme

---
+++ README.md
@@ -0,0 +1,49 @@
+# Pepin
+
+Pepin est un modèle (parmi tant d'autres) de plugin jQuery modulaire.
+
+## Intérêt
+
+* Comporte des paramètres par défaut.
+* Lit les paramètres additionnels ou remplace les valeurs par défaut depuis les attributs `data-*`  HTML de l'élément sur lequel il est appliqué, ou depuis un objet passé en argument à l'initialiation.
+* Peut comporter des méthodes privées/publiques.
+* Accès facile aux paramètres internes et autres méthodes.
+
+## Usage
+
+* Modifier le nom du plugin `pluginName`.
+* Modifier la classe HTML des éléments sur lequel il peut s'appliquer `.js-plugin-elements`.
+* Compléter les paramètres par défaut `defaults`.
+* Compléter les méthodes privées et publiques, notamment la gestion des événements s'il y en a avec `registerEvents`.
+
+```javascript
+$(document).ready(function() {
+
+    // Lie le plugin aux éléments
+    $('.js-plugin-elements').pluginName();
+
+});
+```
+
+### Variantes
+
+```javascript
+$(document).ready(function() {
+
+    // Lie le plugin aux éléments
+    $('.js-plugin-elements').pluginName({'chou': 'croute'});
+
+    // Appel de méthode publique
+    $('.js-plugin-elements').data('pluginName').doSomething();
+
+    // Récupère la valeur de paramètres
+    $('.js-plugin-elements').data('pluginName').settings.parameter;
+
+});
+```
+
+## Inspirations
+
+* <https://github.com/jquery-boilerplate>
+* <http://stefangabos.ro/jquery/jquery-plugin-boilerplate-revisited/>
+
---
+++ plugin.js
@@ -0,0 +1,72 @@
+/**!
+ Plugin
+ Plugin description
+
+ @contributors:
+ @date-created: 2016-02-05
+ */
+
+(function($) {
+
+  $.pluginName = function(el, options) {
+
+    // Default settings values
+    var defaults = {
+      parameter: '' // Parameter description
+    };
+
+    var plugin = this;
+
+    plugin.settings = {};
+
+    var $element = $(el),
+      element = el;
+
+    // Plugin initialization
+    plugin.init = function() {
+      plugin.settings = $.extend({}, defaults, options);
+      updateSettingsFromHTMLData();
+      registerEvents();
+    };
+
+    // Reads plugin settings from HTML data-* attributes (camelCase)
+    var updateSettingsFromHTMLData = function() {
+      var data = $element.data();
+      for (var dat in data) plugin.settings[dat] = data[dat];
+    };
+
+    // Set event handlers on HTML elements (private method)
+    var registerEvents = function() {
+      /* $('.sub-elements',$element).on('click', function(e) {
+        e.preventDefault();
+        plugin.doSomething();
+      }); */
+    };
+
+    // Plugin do something (public method)
+    plugin.doSomething = function() {
+      /*
+      Do something with $element ?
+      and with plugin.settings.parameter ?
+      */
+    };
+
+    // Initialization
+    plugin.init();
+
+  };
+
+  $.fn.pluginName = function(options) {
+
+    return this.each(function() {
+      if (undefined === $(this).data('pluginName')) {
+        var plugin = new $.pluginName(this, options);
+        $(this).data('pluginName', plugin);
+      }
+    });
+
+  };
+
+  $('.js-plugin-elements').pluginName();
+
+})(jQuery);
